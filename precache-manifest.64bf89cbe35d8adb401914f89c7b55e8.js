754c69f0a7d5367a41b75dde05b66dd78661319d
dipesh-om <dipeshkumargupta2@gmail.com>
2019-10-13 17:09:11 +0530

    Updates

---
+++ precache-manifest.64bf89cbe35d8adb401914f89c7b55e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fde85ad79e99ed11d01e142c3ce2e9",
+    "url": "/trackgoal/index.html"
+  },
+  {
+    "revision": "591864d6d28ada834992",
+    "url": "/trackgoal/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1674b0cb8c9fde2b93fc",
+    "url": "/trackgoal/static/js/2.ab418fcb.chunk.js"
+  },
+  {
+    "revision": "591864d6d28ada834992",
+    "url": "/trackgoal/static/js/main.494501e9.chunk.js"
+  },
+  {
+    "revision": "5f057548edc76aa220c0",
+    "url": "/trackgoal/static/js/runtime-main.8ddfcd48.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/trackgoal/static/media/logo.25bf045c.svg"
+  }
+]);
